7db780c2a68811d62cb1dd486add9dd5aed7082d
ming0511 <alsrbdhkalsw@naver.com>
2025-07-24 15:13:50 +0900

    test: 카카오 로그인 테스트 코드 구현

---
+++ src/test/java/gift/kakao/service/KakaoOAuthServiceTest.java
@@ -0,0 +1,22 @@
+package gift.kakao.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@DirtiesContext
+class KakaoOAuthServiceTest {
+
+    @Autowired
+    private KakaoOAuthService kakaoOAuthService;
+
+    @Test
+    void getAuthorizationUrl() {
+        String authUrl = kakaoOAuthService.getAuthorizationUrl("talk_scope");
+        System.out.println(authUrl);
+    }
+}
