ef95cb50f6416b0bc0937405b926537937de5af4
ming0511 <alsrbdhkalsw@naver.com>
2025-07-27 22:40:24 +0900

    test: 전체 테스트 오류 수정(KakaoOAuthControllerTest)

--- src/test/java/gift/kakao/controller/KakaoOAuthControllerTest.java
+++ src/test/java/gift/kakao/controller/KakaoOAuthControllerTest.java
@@ -13,13 +13,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.web.servlet.MockMvc;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class KakaoOAuthControllerTest {
 
     @Autowired
