c9a969fbba2bf167e35052508ff3a8fde646dd9b
ming0511 <alsrbdhkalsw@naver.com>
2025-07-31 12:22:41 +0900

    refactor: 불필요한 변수 제거(ProductServiceImpl)

--- src/main/java/gift/product/service/ProductServiceImpl.java
+++ src/main/java/gift/product/service/ProductServiceImpl.java
@@ -67,10 +67,8 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Product findProductById(Long productId) {
-        Product product = productRepository.findById(productId)
+        return productRepository.findById(productId)
             .orElseThrow(() -> new ProductNotFoundException("존재하지 않는 상품입니다."));
-
-        return product;
     }
 
     @Override
