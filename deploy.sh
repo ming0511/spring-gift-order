4e4e7ef4ef09f303df84359a5969098a2bbd3528
ming0511 <alsrbdhkalsw@naver.com>
2025-08-02 17:16:25 +0900

    refactor: 배포 스크립트 수정
    refactor: 배포 스크립트 수정
    
    refactor: 배포 스크립트 수정
    
    refactor: 배포 스크립트 수정
    
    refactor: 배포 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-BUILD_PATH=$(ls /home/ubuntu/build/*.jar)
+
+PROJECT_ROOT=$(pwd)
+
+git checkout step
+
+./gradlew clean build
+
+BUILD_PATH=$(ls ${PROJECT_ROOT}/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_PATH)
 
 CURRENT_PID=$(pgrep -f $JAR_NAME)
@@ -12,9 +19,10 @@ else
   sleep 5
 fi
 
-DEPLOY_PATH=/home/ubuntu/
+DEPLOY_PATH=~/deploy/spring-gift-order
+
 cp $BUILD_PATH $DEPLOY_PATH
 cd $DEPLOY_PATH
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+DEPLOY_JAR=$DEPLOY_PATH/$JAR_NAME
 nohup java -jar $DEPLOY_JAR > /dev/null 2> /dev/null < /dev/null &
